df7e1caec0c3166196a5da08c292740ca0bceb0d
Volodymyr Hotsyk <gotsyk@gmail.com>
2015-03-22 23:23:53 +0200

    Set correct assets paths

--- vulyk_declaration/models/tasks.py
+++ vulyk_declaration/models/tasks.py
@@ -33,10 +33,16 @@ class DeclarationTaskType(AbstractTaskType):
 
     redundancy = 3
 
-    JS_ASSETS = ["static/scripts/keymaster.js",
-                 "static/scripts/handlebars.js",
-                 "static/scripts/bootstrap-select.js",
-                 "static/scripts/base.js"]
+    JS_ASSETS = ["static/scripts/main.js",
+                 "static/scripts/messages_uk.min.js",
+                 "static/scripts/html5shiv.js",
+                 "static/scripts/jquery-cloneya.min.js",
+                 "static/scripts/jquery-ui.min.js",
+                 "static/scripts/jquery.dateSelectBoxes.js",
+                 "static/scripts/jquery.min.js",
+                 "static/scripts/jquery.placeholder.min.js",
+                 "static/scripts/jquery.serializejson.js",
+                 "static/scripts/jquery.validate.min.js"]
 
-    CSS_ASSETS = ["static/styles/bootstrap-select.css",
-                  "static/styles/base.css"]
+    CSS_ASSETS = ["static/styles/core-style.css",
+                  "static/styles/style.css"]
