d6ecf114082c200c4522733419adb993a47be23b
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2015-03-23 00:10:49 +0200

    Media re-ordered in a right order, misc changes to html

--- vulyk_declaration/models/tasks.py
+++ vulyk_declaration/models/tasks.py
@@ -33,16 +33,15 @@ class DeclarationTaskType(AbstractTaskType):
 
     redundancy = 3
 
-    JS_ASSETS = ["static/scripts/main.js",
-                 "static/scripts/messages_uk.min.js",
-                 "static/scripts/html5shiv.js",
-                 "static/scripts/jquery-cloneya.min.js",
+    JS_ASSETS = ["static/scripts/html5shiv.js",
                  "static/scripts/jquery-ui.min.js",
+                 "static/scripts/jquery.validate.min.js",
+                 "static/scripts/messages_uk.min.js",
+                 "static/scripts/jquery-cloneya.min.js",
                  "static/scripts/jquery.dateSelectBoxes.js",
-                 "static/scripts/jquery.min.js",
                  "static/scripts/jquery.placeholder.min.js",
                  "static/scripts/jquery.serializejson.js",
-                 "static/scripts/jquery.validate.min.js"]
+                 "static/scripts/main.js"]
 
     CSS_ASSETS = ["static/styles/core-style.css",
                   "static/styles/style.css"]
