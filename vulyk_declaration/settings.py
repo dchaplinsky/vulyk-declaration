7ca73b9d8d47e9d4fc7f391a0b46b23d784b23e3
Volodymyr Hotsyk <gotsyk@gmail.com>
2015-03-22 21:52:40 +0200

    Added plugin settings file

---
+++ vulyk_declaration/settings.py
@@ -0,0 +1,9 @@
+TASK_NAME = 'DeclarationTask'
+
+JS_ASSETS = ['']
+JS_ASSETS_OUTPUT = 'scripts/vulyk-declaration.js'
+JS_ASSETS_FILTERS = 'yui_js'
+
+CSS_ASSETS = ['']
+CSS_ASSETS_OUTPUT = 'styles/vulyk-declaration.css'
+CSS_ASSETS_FILTERS = 'yui_css'
