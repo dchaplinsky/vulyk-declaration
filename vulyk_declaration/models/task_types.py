b08b8fe7ee6b6e0330181653e71f4db47a4174fa
Dmitry Gambal <dgambal@conversionscience.co.uk>
2015-03-31 03:36:01 +0300

    - added stubs for name and description

--- vulyk_declaration/models/task_types.py
+++ vulyk_declaration/models/task_types.py
@@ -1,3 +1,6 @@
+# -*- coding=utf-8 -*-
+from __future__ import unicode_literals
+
 from vulyk.models.task_types import AbstractTaskType
 
 from vulyk_declaration.models.tasks import DeclarationAnswer, DeclarationTask
@@ -10,21 +13,24 @@ class DeclarationTaskType(AbstractTaskType):
     answer_model = DeclarationAnswer
     task_model = DeclarationTask
 
-    template = "index.html"
-    helptext_template = "help.html"
-    type_name = "declaration_task"
+    name = 'Декларации'
+    description = 'Распознавание деклараций'
+
+    template = 'index.html'
+    helptext_template = 'help.html'
+    type_name = 'declaration_task'
 
     redundancy = 3
 
-    JS_ASSETS = ["static/scripts/jquery-ui.min.js",
-                 "static/scripts/jquery.validate.min.js",
-                 "static/scripts/messages_uk.min.js",
-                 "static/scripts/jquery-cloneya.min.js",
-                 "static/scripts/jquery.dateSelectBoxes.js",
-                 "static/scripts/jquery.placeholder.min.js",
-                 "static/scripts/jquery.serializejson.js",
-                 "static/scripts/data.js",
-                 "static/scripts/main.js"]
+    JS_ASSETS = ['static/scripts/jquery-ui.min.js',
+                 'static/scripts/jquery.validate.min.js',
+                 'static/scripts/messages_uk.min.js',
+                 'static/scripts/jquery-cloneya.min.js',
+                 'static/scripts/jquery.dateSelectBoxes.js',
+                 'static/scripts/jquery.placeholder.min.js',
+                 'static/scripts/jquery.serializejson.js',
+                 'static/scripts/data.js',
+                 'static/scripts/main.js']
 
-    CSS_ASSETS = ["static/styles/core-style.css",
-                  "static/styles/style.css"]
+    CSS_ASSETS = ['static/styles/core-style.css',
+                  'static/styles/style.css']
